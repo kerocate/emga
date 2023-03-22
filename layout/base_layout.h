3b8f7deff7e3762d50f099458789170aa57ccdd8
Kerocate <kerocate_moe@outlook.com>
2023-03-23 01:03:12 +0800

    撸样式，整理文件结构

--- layout/base_layout.h
+++ layout/base_layout.h
@@ -8,22 +8,23 @@
 
 lv_obj_t *base_layout(void)
 {
-    //bg
+    // bg
     lv_obj_t *cont = lv_obj_create(lv_scr_act());
+    lv_obj_remove_style_all(cont);
     lv_obj_clear_flag(cont, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_set_size(cont, 800, 480);
     lv_obj_center(cont);
     lv_obj_set_flex_flow(cont, LV_FLEX_FLOW_ROW);
-    lv_obj_add_style(cont,&default_style,0);
+    lv_obj_add_style(cont, &default_style, 0);
 
-    //title
+    // title
     lv_obj_t *obj;
     obj = lv_obj_create(cont);
+    lv_obj_remove_style_all(obj);
     lv_obj_clear_flag(obj, LV_OBJ_FLAG_SCROLLABLE);
     lv_obj_set_size(obj, 100, 480); /*Fix size*/
+    lv_obj_add_style(obj, &default_style, 0);
     lv_obj_add_style(obj, &title_style, 0);
-
-
 
     return cont;
 }
--- style/base_style.h
+++ style/base_style.h
@@ -15,10 +15,9 @@ void init_base_style()
 
     // title
     lv_style_init(&title_style);
+    lv_style_set_bg_opa(&title_style, LV_OPA_100);
     lv_style_set_bg_color(&title_style, lv_palette_main(LV_PALETTE_LIGHT_BLUE));
     lv_style_set_shadow_width(&title_style, 10);
     lv_style_set_shadow_ofs_x(&title_style, 5);
     lv_style_set_shadow_color(&title_style, lv_palette_main(LV_PALETTE_BLUE));
-
-    
 }
